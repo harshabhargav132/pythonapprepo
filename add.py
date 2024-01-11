934151d57789cd41090e639cbee84789f384117d
harshabhargav132 <2200090009@kluniversity.in>
2024-01-11 14:05:07 +0530

    Thi is version1 file

---
+++ add.py
@@ -0,0 +1,8 @@
+This is a c program python ile[D[D[Dfile
+int su1,sum2[D[D[D[D[D[Dm[1,sum2,sum;
+scanf &sum1,&sum2
+print enter the values os sum1 and sum2
+sum = sum1+sum2
+print sum[D[D[D[Df sum
+
+
